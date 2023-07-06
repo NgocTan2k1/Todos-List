581293a9a7b2d417739186cd3c76f26c641745cf
NgocTan <phamngoctan2016@gmail.com>
2023-07-07 00:17:25 +0700

    WFH

--- src/components/TestTodo/index.js
+++ src/components/TestTodo/index.js
@@ -1,81 +1,83 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect, useState } from "react";
 import { collection, getDocs, addDoc, getFirestore } from "firebase/firestore";
-import { app } from '../../firebase';
-import "./test.css"
+import { app } from "../../firebase";
+import "./test.css";
+import { List, Skeleton } from "antd";
 const Todo = () => {
-    const [todo, setTodo] = useState("");
-    const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [data, setData] = useState([]);
+  const [list, setList] = useState([]);
 
-    const db = getFirestore(app);
+  const db = getFirestore(app);
 
-    const fetchPost = async () => {
+  const fetchPost = async () => {
+    // await getDocs(collection(db, `users`))
+    //     .then((querySnapshot) => {
+    //         const newUsers = querySnapshot.docs.map((doc) => doc.data())
+    //         setTodos(newUsers[0].userEmails)
+    //     })
+    //     .catch(error => console.log(error))
 
-        // await getDocs(collection(db, `users`))
-        //     .then((querySnapshot) => {
-        //         const newUsers = querySnapshot.docs.map((doc) => doc.data())
-        //         setTodos(newUsers[0].userEmails)
-        //     })
-        //     .catch(error => console.log(error))
+    await getDocs(collection(db, "todos")).then((querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setTodos(newData);
+      console.log("newData:", newData);
+    });
+  };
 
-        await getDocs(collection(db, "todos"))
-            .then((querySnapshot) => {
-                const newData = querySnapshot.docs
-                    .map((doc) => ({ ...doc.data(), id: doc.id }));
-                setTodos(newData);
-                console.log(todos, newData);
-            })
+  useEffect(() => {
+    fetchPost();
+    fetch(`https://randomuser.me/api/?results=${10}&inc=name,gender,email,nat,picture&noinfo`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.results);
+        setList(res.results);
+        console.log(res.results)
+      });
+    console.log("todos1:", todos);
 
+  }, []);
 
+  const addTodo = async (e) => {
+    e.preventDefault();
+
+    try {
+      const docRef = await addDoc(collection(db, "todos"), {
+        todo: todo,
+      });
+      console.log("Document written with ID:", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
+  };
 
-    useEffect(() => {
-        fetchPost();
-        console.log("todos1:", todos);
-    }, [])
+  return (
+    <section className="todo-container">
+      <div className="todo">
+        <h1 className="header">Todo-App</h1>
 
-    const addTodo = async (e) => {
-        e.preventDefault();
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="What do you have to do today?"
+              onChange={(e) => setTodo(e.target.value)}
+            />
+          </div>
 
-        try {
-            const docRef = await addDoc(collection(db, "todos"), {
-                todo: todo,
-            });
-            console.log("Document written with ID:", docRef.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
-    }
+          <div className="btn-container">
+            <button type="submit" className="btn" onClick={addTodo}>
+              Submit
+            </button>
+          </div>
+        </div>
 
-    return (
-        <section className="todo-container">
-            <div className="todo">
-                <h1 className="header">
-                    Todo-App
-                </h1>
-
-                <div>
-
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="What do you have to do today?"
-                            onChange={(e) => setTodo(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="btn-container">
-                        <button
-                            type="submit"
-                            className="btn"
-                            onClick={addTodo}
-                        >
-                            Submit
-                        </button>
-                    </div>
-
-                </div>
-
-                {/* <div className="todo-content">
+        {/* <div className="todo-content">
                     {
                         todos?.map((todo, i) => (
                             <p key={i}>
@@ -85,19 +87,36 @@ const Todo = () => {
                     }
                 </div> */}
 
-                <div className="todo-content">
-                    {
-                        todos?.map((todo, i) => (
-                            <p key={i}>
-                                {todo.todo}
-                            </p>
-                        ))
-                    }
-                </div>
+        <div className="todo-content">
+          {todos?.map((todo, i) => (
+            <p key={i}>{todo.todo}</p>
+          ))}
+        </div>
 
-            </div>
-        </section>
-    )
-}
+        <List
+          className="demo-loadmore-list"
+          itemLayout="horizontal"
+          dataSource={list}
+          renderItem={(item) => (
+            <List.Item
+              actions={[
+                <button key="list-loadmore-edit">Edit Deadline</button>,
+                <button key="list-loadmore-more">Edit Assign</button>,
+              ]}
+            >
+              <Skeleton title={false} loading={item.loading} active>
+                <List.Item.Meta
+                  title={item.name?.last}
+                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                />
+                <div>New</div>
+              </Skeleton>
+            </List.Item>
+          )}
+        />
+      </div>
+    </section>
+  );
+};
 
 export default Todo;
--- src/components/TodosList/index.js
+++ src/components/TodosList/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/alt-text */
 import { getAuth, signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { collection, getDocs, addDoc, getFirestore } from "firebase/firestore";
-import { Checkbox, Button, Modal, Row, Col } from 'antd';
-
+import { Checkbox, Button, Modal, Row, Col } from "antd";
 
 import classNames from "classnames/bind";
 import styles from "./TodosList.module.scss";
@@ -46,218 +48,254 @@ const cx = classNames.bind(styles);
 // ]
 
 function TodosList() {
-    const [content, setContent] = useState("");
-    const [deadline, setDeadline] = useState("");
-    const [assign, setAssign] = useState([]);
-    const [add, setAdd] = useState(false);
+  const [content, setContent] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [assign, setAssign] = useState([]);
+  const [add, setAdd] = useState(false);
 
-    const [todos, setTodos] = useState([]);
-    const [users, setUsers] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isNotice, setIsNotice] = useState(false);
-    const [notice, setNotice] = useState("");
-    const [state, setState] = useState(true)
+  const [todos, setTodos] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isNotice, setIsNotice] = useState(false);
+  const [notice, setNotice] = useState("");
+  const [state, setState] = useState(true);
 
-    const navigate = useNavigate();
-    const auth = getAuth(app);
-    const db = getFirestore(app);
+  const navigate = useNavigate();
+  const auth = getAuth(app);
+  const db = getFirestore(app);
 
-    const [user, loading, error] = useAuthState(auth);
-    if (!loading) {
-        if (!user) {
-            navigate("/");
-        }
+  const [user, loading, error] = useAuthState(auth);
+  if (!loading) {
+    if (!user) {
+      navigate("/");
     }
+  }
 
-    // getdata function
-    const fetchData = async () => {
+  // getdata function
+  const fetchData = async () => {
+    await getDocs(collection(db, `todos`))
+      .then((querySnapshot) => {
+        const newTodos = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        console.log("newTodos:", newTodos);
+        setTodos(newTodos);
+        console.log("todos", todos);
+      })
+      .catch((error) => console.log(error));
 
-        await getDocs(collection(db, `todos`))
-            .then((querySnapshot) => {
-                const newTodos = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-                console.log("newTodos:", newTodos);
-                setTodos(newTodos);
-                console.log("todos", todos)
-            })
-            .catch(error => console.log(error))
+    await getDocs(collection(db, `users`))
+      .then((querySnapshot) => {
+        console.log(querySnapshot.docs);
+        const newUsers = querySnapshot.docs.map((doc) => doc.data());
+        console.log("userEmails:", newUsers[0].userEmails);
+        setUsers(newUsers[0].userEmails);
+        console.log("users: ", users);
+      })
+      .catch((error) => console.log(error));
+  };
 
-        await getDocs(collection(db, `users`))
-            .then((querySnapshot) => {
-                console.log(querySnapshot.docs)
-                const newUsers = querySnapshot.docs.map((doc) => doc.data())
-                console.log("userEmails:", newUsers[0].userEmails);
-                setUsers(newUsers[0].userEmails)
-                console.log("users: ", users)
-            })
-            .catch(error => console.log(error))
+  //getdata each update or the first time
+  useEffect(() => {
+    fetchData();
+  }, [add]);
 
+  // function log out
+  const Logout = () => {
+    signOut(auth);
+    navigate("/");
+  };
+
+  const handleCancelNotice = () => {
+    setIsNotice(false);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setAssign(assign);
+    setIsModalOpen(false);
+  };
+
+  const handleKeyDown = async (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      await handleAddTodo();
     }
+  };
 
-    //getdata each update or the first time
-    useEffect(() => {
-        fetchData();
-    }, [add])
+  const handleChangeContent = (e) => {
+    setContent(e.target.value);
+  };
 
-    // function log out
-    const Logout = () => {
-        signOut(auth);
-        navigate("/");
-    }
+  const handleChangeDeadline = (e) => {
+    const dl = new Date(e.target.value);
+    setDeadline(`${dl.getDate()}-${dl.getMonth() + 1}-${dl.getFullYear()}`);
+  };
 
-    const handleCancelNotice = () => {
-        setIsNotice(false);
-    };
+  const onChange = (checkedValues) => {
+    setAssign(checkedValues);
+  };
 
-    const showModal = () => {
-        setIsModalOpen(true);
-    };
+  const handleCheck = () => {
+    setState(!state);
+    console.log("click");
+  };
 
-    const handleOk = () => {
-        setAssign(assign)
-        setIsModalOpen(false);
-    };
+  const handleAddTodo = async () => {
+    const date = new Date();
 
-
-    const handleKeyDown = async (e) => {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            await handleAddTodo();
-        }
-    }
-
-
-    const handleChangeContent = (e) => {
-        setContent(e.target.value)
-    }
-
-    const handleChangeDeadline = (e) => {
-        const dl = new Date(e.target.value);
-        setDeadline(`${dl.getDate()}-${dl.getMonth() + 1}-${dl.getFullYear()}`);
-    }
-
-    const onChange = (checkedValues) => {
-        setAssign(checkedValues);
-    };
-
-    const handleCheck = () => {
-        setState(!state);
-        console.log("click")
-    };
-
-
-
-    const handleAddTodo = async () => {
-        const date = new Date();
-
-        if (!content) {
-            setIsNotice(true);
-            setNotice("Please enter the content");
+    if (!content) {
+      setIsNotice(true);
+      setNotice("Please enter the content");
+    } else {
+      if (assign.length === 0) {
+        setIsNotice(true);
+        setNotice("Please enter the assign this for some people");
+      } else {
+        if (!deadline) {
+          setIsNotice(true);
+          setNotice("Please enter the deadline");
         } else {
-            if (assign.length === 0) {
-                setIsNotice(true);
-                setNotice("Please enter the assign this for some people");
-            } else {
-                if (!deadline) {
-                    setIsNotice(true);
-                    setNotice("Please enter the deadline");
-                } else {
-                    console.log("Add todo:");
-                    console.log("id:", todos.length + 1);
-                    console.log("content:", content);
-                    console.log("deadline: ", deadline);
-                    console.log("createDate: ", `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`);
-                    console.log("creator:", user.email);
-                    console.log("assignNames:", assign);
-                    console.log("status:", false);
+          console.log("Add todo:");
+          console.log("id:", todos.length + 1);
+          console.log("content:", content);
+          console.log("deadline: ", deadline);
+          console.log(
+            "createDate: ",
+            `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+          );
+          console.log("creator:", user.email);
+          console.log("assignNames:", assign);
+          console.log("status:", false);
 
-                    await addDoc(collection(db, "todos"), {
-                        id: todos.length + 1,
-                        title: content,
-                        deadline,
-                        deadline: `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`,
-                        creator: user.email,
-                        assignNames: assign,
-                        status: false,
-                    });
-                    setAdd(!add);
-                    setContent("");
-                    setAssign([]);
-                }
-            }
+          await addDoc(collection(db, "todos"), {
+            id: todos.length + 1,
+            title: content,
+            deadline: `${date.getDate()}-${
+              date.getMonth() + 1
+            }-${date.getFullYear()}`,
+            creator: user.email,
+            assignNames: assign,
+            status: false,
+          });
+          setAdd(!add);
+          setContent("");
+          setAssign([]);
         }
+      }
     }
+  };
 
-    return (
-        <div className={cx("wrapper")}>
-            <Modal title="Let choice some people for this work" open={isModalOpen} onOk={handleOk} closable={false} cancelButtonProps={{ style: { display: 'none' } }}>
-                <Checkbox.Group
-                    style={{
-                        width: '100%',
-                    }}
-                    onChange={onChange}
-                >
-                    <Row>
-                        {
-                            users.map((user, i) => (
-                                <Col key={i} span={8}>
-                                    <Checkbox value={user}>{user}</Checkbox>
-                                </Col>
-                            ))
-                        }
-                    </Row>
-                </Checkbox.Group>
-            </Modal>
+  return (
+    <div className={cx("wrapper")}>
+      <Modal
+        title="Let choice some people for this work"
+        open={isModalOpen}
+        onOk={handleOk}
+        closable={false}
+        cancelButtonProps={{ style: { display: "none" } }}
+      >
+        <Checkbox.Group
+          style={{
+            width: "100%",
+          }}
+          onChange={onChange}
+        >
+          <Row>
+            {users.map((user, i) => (
+              <Col key={i} span={8}>
+                <Checkbox value={user}>{user}</Checkbox>
+              </Col>
+            ))}
+          </Row>
+        </Checkbox.Group>
+      </Modal>
 
-            <Modal title="Notice" open={isNotice} cancelButtonProps={{ style: { display: 'none' } }} onOk={handleCancelNotice} closable={false}>
-                {notice}
-            </Modal>
+      <Modal
+        title="Notice"
+        open={isNotice}
+        cancelButtonProps={{ style: { display: "none" } }}
+        onOk={handleCancelNotice}
+        closable={false}
+      >
+        {notice}
+      </Modal>
 
-            <div className={cx("todo-app")}>
-                <h1 className={cx("title")}>Todos List of the Internship Programing <img className={cx("img-icon")} src={img} /></h1>
+      <div className={cx("todo-app")}>
+        <h1 className={cx("title")}>
+          Todos List of the Internship Programing{" "}
+          <img className={cx("img-icon")} src={img} />{" "}
+        </h1>
 
-                <div className={cx("form-input")}>
-                    <div className={cx("item-input-content", "item-input")}>
-                        <label className={cx("label-input", "label-input-content")}>Content</label>
-                        <input value={content} onChange={(e) => handleChangeContent(e)} onKeyDown={(e) => handleKeyDown(e)} type="text" className={cx("input")} placeholder="Enter your work" />
-                    </div>
+        <div className={cx("form-input")}>
+          <div className={cx("item-input-content", "item-input")}>
+            <label className={cx("label-input", "label-input-content")}>
+              Content
+            </label>
+            <input
+              value={content}
+              onChange={(e) => handleChangeContent(e)}
+              onKeyDown={(e) => handleKeyDown(e)}
+              type="text"
+              className={cx("input")}
+              placeholder="Enter your work"
+            />
+          </div>
 
-                    <div className={cx("item-input-date", "item-input")}>
-                        <label className={cx("label-input", "label-input-date")}>Deadline</label>
-                        <input onChange={(e) => handleChangeDeadline(e)} type="date" className={cx("input", "input-date")} />
-                    </div>
+          <div className={cx("item-input-date", "item-input")}>
+            <label className={cx("label-input", "label-input-date")}>
+              Deadline
+            </label>
+            <input
+              onChange={(e) => handleChangeDeadline(e)}
+              type="date"
+              className={cx("input", "input-date")}
+            />
+          </div>
 
-                    <div className={cx("item-input-assign", "item-input")}>
-                        <Button rootClassName={cx("label-input", "label-input-assign")} type="primary" onClick={showModal}>
-                            Assign
-                        </Button>
-                    </div>
+          <div className={cx("item-input-assign", "item-input")}>
+            <Button
+              rootClassName={cx("label-input", "label-input-assign")}
+              type="primary"
+              onClick={showModal}
+            >
+              Assign
+            </Button>
+          </div>
 
-                    <button onClick={handleAddTodo} className={cx("btn-input")}>Add</button>
-                </div>
+          <button onClick={handleAddTodo} className={cx("btn-input")}>
+            Add
+          </button>
+        </div>
 
-                <div className={cx("container-task")}>
-                    <ul className={cx("list-task")}>
-
-                        {
-                            todos?.map((todo, i) => (
-                                (todo.assignNames.includes(user.email) || (user.email === todo.creator)) ?
-                                    todo.status ? ((<li onClick={handleCheck} key={i} className={cx("task", "unchecked")}>
-                                        STT: {i} - Title:{todo?.title} - Creator: {todo?.creator} - Assign: {todo?.assignNames}
-                                    </li>)) : ((<li key={i} className={cx("task", "checked")}>
-                                        STT: {i} - Title:{todo?.title} - Creator: {todo?.creator} - Assign: {todo?.assignNames}
-                                    </li>))
-                                    :
-                                    ""
-                            ))
-                        }
-
-                    </ul>
-                </div>
-            </div>
-            <button onClick={() => Logout()}>Logout</button>
-        </div >
-
-    )
+        <div className={cx("container-task")}>
+          <ul className={cx("list-task")}>
+            {todos?.map((todo, i) => (
+              <div key={i} className={cx("task")}>
+                <div>Task-{i+1}</div> 
+                <div>Title:{todo?.title}</div>
+                Creator: {todo?.creator} - Deadline: {todo?.deadline}
+                <div>Assign: {todo?.assignNames}</div>
+              </div>
+              // (todo.assignNames.includes(user.email) || (user.email === todo.creator)) ?
+              //     todo.status ? ((<li onClick={handleCheck} key={i} className={cx("task", "unchecked")}>
+              //         STT: {i} - Title:{todo?.title} - Creator: {todo?.creator} - Assign: {todo?.assignNames}
+              //     </li>)) : ((<li key={i} className={cx("task", "checked")}>
+              //         STT: {i} - Title:{todo?.title} - Creator: {todo?.creator} - Assign: {todo?.assignNames}
+              //     </li>))
+              //     :
+              //     ""
+            ))}
+          </ul>
+        </div>
+      </div>
+      <button onClick={() => Logout()}>Logout</button>
+    </div>
+  );
 }
 
 export default TodosList;
